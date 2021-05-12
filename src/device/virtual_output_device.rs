cd98d563f4f797a0eea70657d0e67bb86d3cba5c
shiro <shiro@usagi.io>
2021-05-12 15:37:36 +0200

    Add custom error message when failing to write to /dev/uinput

--- src/device/virtual_output_device.rs
+++ src/device/virtual_output_device.rs
@@ -12,8 +12,15 @@ pub async fn init_virtual_output_device(
     virt_device::init_virtual_device(&mut new_device)
         .map_err(|err| anyhow!("failed to instantiate udev device: {}", err))?;
 
-    let input_device = UInputDevice::create_from_device(&new_device)
-        .map_err(|err| anyhow!("failed to initialize uinput device: {}", err))?;
+    let input_device = UInputDevice::create_from_device(&new_device);
+
+    if let Err(err) = &input_device {
+        if err.kind() == io::ErrorKind::PermissionDenied {
+            return Err(anyhow!("failed to obtain write access to '/dev/uinput': {}", err));
+        }
+    };
+
+    let input_device = input_device.map_err(|err| anyhow!("failed to initialize uinput device: {}", err))?;
 
     task::spawn(async move {
         loop {
