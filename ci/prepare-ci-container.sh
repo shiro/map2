13ca55496846abbe179570dab3bd7704ac957541
shiro <shiro@usagi.io>
2024-06-03 22:36:03 +0900

    Fix arm CI

--- ci/prepare-ci-container.sh
+++ ci/prepare-ci-container.sh
@@ -13,9 +13,7 @@ if command -v apt-get &> /dev/null; then
     deb [arch=arm64] http://ports.ubuntu.com/ jammy-backports main multiverse universe
     deb [arch=arm64] http://ports.ubuntu.com/ jammy-updates main multiverse universe
 EOF
-    dpkg --print-foreign-architectures
     dpkg --add-architecture arm64
-    dpkg --print-foreign-architectures
     set +e
     apt-get update
     set -e
@@ -23,9 +21,10 @@ EOF
     dpkg -L libxkbcommon-dev:arm64
     export PATH=~/usr/lib/aarch64-linux-gnu:$PATH
     export RUSTFLAGS='-L /usr/lib/aarch64-linux-gnu'
-    cp /usr/lib/aarch64-linux-gnu/libxkbcommon.so /usr/aarch64-unknown-linux-gnu/aarch64-unknown-linux-gnu/lib64
-    cp /usr/lib/aarch64-linux-gnu/libxkbcommon.so.0 /usr/aarch64-unknown-linux-gnu/aarch64-unknown-linux-gnu/lib64
-    cp /usr/lib/aarch64-linux-gnu/libxkbcommon.so.0.0.0 /usr/aarch64-unknown-linux-gnu/aarch64-unknown-linux-gnu/lib64
+    # hack for maturin wheel repair not picking up rust flags
+    # https://github.com/PyO3/maturin/discussions/2092#discussioncomment-9648400
+    cp /usr/lib/aarch64-linux-gnu/libxkbcommon.so.0.0.0 /usr/aarch64-unknown-linux-gnu/aarch64-unknown-linux-gnu/sysroot/lib64/libxkbcommon.so
+    cp /usr/lib/aarch64-linux-gnu/libxkbcommon.so.0.0.0 /usr/aarch64-unknown-linux-gnu/aarch64-unknown-linux-gnu/sysroot/lib64/libxkbcommon.so.0
 elif command -v yum &> /dev/null; then
     echo "using yum"
     yum install -y libxkbcommon-devel libatomic
