5caf293a43940b89d4426f2d76c5ff14535e67ea
shiro <shiro@usagi.io>
2025-12-28 16:39:34 +0900

    Fix nix missing dep

--- shell.nix
+++ shell.nix
@@ -1,42 +1,59 @@
-{ pkgs ? import <nixpkgs> { } }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 
-let 
-    python = with pkgs; python312.withPackages (python-pkgs: with python-pkgs; []);
-    lib-path = with pkgs; pkgs.lib.makeLibraryPath [ libxkbcommon libevdev udev python ];
-
-    rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
-    pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
-    rust = pkgs.rust-bin.stable."1.91.1".default.override {
-        extensions = [ "rust-src" "rust-analyzer" ];
-    };
-in
-pkgs.mkShell { 
-    buildInputs = [
-      rust
-    ] ++ (with pkgs; [
-       libevdev udev
-    ]);
-    nativeBuildInputs = with pkgs; [ pkg-config libxkbcommon libevdev udev ];
-    packages = with pkgs; [
-        automake
-        autoconf
-        udev
-        libevdev
-        python
+let
+  python = with pkgs; python312.withPackages (python-pkgs: with python-pkgs; [ ]);
+  lib-path =
+    with pkgs;
+    pkgs.lib.makeLibraryPath [
+      libxkbcommon
+      libevdev
+      libcap
+      udev
+      python
     ];
 
-    shellHook = ''
-        export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${ lib-path }"
+  rust_overlay = import (
+    builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"
+  );
+  pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+  rust = pkgs.rust-bin.stable."1.91.1".default.override {
+    extensions = [
+      "rust-src"
+      "rust-analyzer"
+    ];
+  };
+in
+pkgs.mkShell {
+  buildInputs = [
+    rust
+  ]
+  ++ (with pkgs; [
+    libevdev
+    udev
+  ]);
+  nativeBuildInputs = with pkgs; [ pkg-config ];
+  packages = with pkgs; [
+    automake
+    autoconf
+    udev
+    libevdev
+    python
+  ];
 
-        # Setup the virtual environment if it doesn't already exist.
-        VENV=venv
-        if test ! -d $VENV; then
-            python -m venv $VENV
-            source ./$VENV/bin/activate
-            pip install -r requirements.txt
-        else
-            source ./$VENV/bin/activate
-        fi
-    '';
-    RUST_BACKTRACE = 1;
+  shellHook = ''
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${lib-path}"
+
+    # Setup the virtual environment if it doesn't already exist.
+    VENV=venv
+    if test ! -d $VENV; then
+        python -m venv $VENV
+        source ./$VENV/bin/activate
+        pip install -r requirements.txt
+    else
+        source ./$VENV/bin/activate
+    fi
+  '';
+  RUST_BACKTRACE = 1;
 }
